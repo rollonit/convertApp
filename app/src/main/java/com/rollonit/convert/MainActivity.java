ca30d6555e9f58c72efc5df3f8e45b3b1132acb0
rollonit <46538361+rollonit@users.noreply.github.com>
2023-04-13 23:17:50 +0800

    Changed Visual Look and Implemented Helper
    - added more contrast the the home page
    - changed the methods in the main activity to use the new helper class
    - changed the font on the inputs to Cancadia Code, which is monospace

--- app/src/main/java/com/rollonit/convert/MainActivity.java
+++ app/src/main/java/com/rollonit/convert/MainActivity.java
@@ -1,5 +1,8 @@
 package com.rollonit.convert;
 
+import static com.rollonit.convert.helper.Converter.convert;
+import static com.rollonit.convert.helper.Converter.convertTextToUnit;
+
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,37 +15,50 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.rollonit.convert.helper.Unit;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
+    EditText e1, e2;
+    Spinner s1, s2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Setup for decimal format
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.US);
+        DecimalFormat df = new DecimalFormat("#.###", otherSymbols);
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Setup for first input value selector
-        Spinner s1 = findViewById(R.id.spinner);
+        s1 = findViewById(R.id.spinner);
 
-        ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, R.array.unit_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, R.array.length_units, android.R.layout.simple_spinner_item);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_item);
         s1.setAdapter(aa);
         s1.setOnItemSelectedListener(this);
 
         // Setup for second input value selector
-        Spinner s2 = findViewById(R.id.spinner2);
+        s2 = findViewById(R.id.spinner2);
 
-        ArrayAdapter<CharSequence> aa2 = ArrayAdapter.createFromResource(this, R.array.unit_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> aa2 = ArrayAdapter.createFromResource(this, R.array.length_units, android.R.layout.simple_spinner_item);
         aa2.setDropDownViewResource(android.R.layout.simple_spinner_item);
         s2.setAdapter(aa2);
         s2.setOnItemSelectedListener(this);
 
         //setup for unit conversion text fields
-        EditText e1 = findViewById(R.id.input);
-        EditText e2 = findViewById(R.id.output);
+        e1 = findViewById(R.id.input);
+        e2 = findViewById(R.id.output);
 
+        // Handle events from the text fields
         e1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -51,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //
+                // Do nothing
             }
 
             @Override
@@ -59,12 +75,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 if (!e2.hasFocus()) {
                     try {
                         if (e1.getText().toString().equals("")) {
-                            e2.setText("0");
+                            e2.setText("");
                             return;
                         }
                         double val = Double.parseDouble(e1.getText().toString());
                         Unit from = convertTextToUnit(s1.getSelectedItem().toString()), to = convertTextToUnit(s2.getSelectedItem().toString());
-                        e2.setText(String.format(Locale.ENGLISH, "%.2f", convert(val, from, to)));
+                        e2.setText(df.format(convert(val, from, to)));
                     } catch (NumberFormatException e) {
                         //do nothing
                     }
@@ -80,6 +96,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
                 if (!e1.hasFocus()) {
                     try {
                         if (e2.getText().toString().equals("")) {
@@ -88,19 +109,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         }
                         double val = Double.parseDouble(e2.getText().toString());
                         Unit from = convertTextToUnit(s2.getSelectedItem().toString()), to = convertTextToUnit(s1.getSelectedItem().toString());
-                        e1.setText(String.format(Locale.ENGLISH, "%.2f", convert(val, from, to)));
+                        e1.setText(df.format(convert(val, from, to)));
                     } catch (NumberFormatException e) {
                         //do nothing
                     }
                 }
             }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // Do something
-            }
         });
     }
+
+    // Handle events from the spinners
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -117,121 +135,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             // for debugging TODO remove later
             Toast.makeText(getApplicationContext(), "Selected item in spinner 2: " + selectedItem2, Toast.LENGTH_SHORT).show();
         }
-        // Add more if statements if you have more spinners
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
         // Do nothing
-    }
-
-    /**
-     * This method is used to convert the input value from one unit of measurement to another.
-     *
-     * @param value The value to be converted.
-     * @param from  The unit of measurement that the value is currently in.
-     * @param to    The unit of measurement that the value should be converted to.
-     * @return The converted value.
-     */
-    public double convert(double value, Unit from, Unit to) {
-        double meters = 0;
-
-        // Convert the value to meters
-        switch (from) {
-            case METER:
-                meters = value;
-                break;
-            case KILOMETER:
-                meters = value * 1000;
-                break;
-            case CENTIMETER:
-                meters = value / 100;
-                break;
-            case MILLIMETER:
-                meters = value / 1000;
-                break;
-            case MILE:
-                meters = value * 1609.344;
-                break;
-            case YARD:
-                meters = value * 0.9144;
-                break;
-            case FOOT:
-                meters = value * 0.3048;
-                break;
-            case INCH:
-                meters = value * 0.0254;
-                break;
-            case NAUTICAL_MILE:
-                meters = value * 1852;
-                break;
-        }
-
-        // Convert and return the value from meters to the desired unit
-        switch (to) {
-            case METER:
-                return meters;
-            case KILOMETER:
-                return meters / 1000;
-            case CENTIMETER:
-                return meters * 100;
-            case MILLIMETER:
-                return meters * 1000;
-            case MILE:
-                return meters / 1609.344;
-            case YARD:
-                return meters / 0.9144;
-            case FOOT:
-                return meters / 0.3048;
-            case INCH:
-                return meters / 0.0254;
-            case NAUTICAL_MILE:
-                return meters / 1852;
-            default:
-                return -1;
-        }
-    }
-
-    /**
-     * This method is used to convert the text in the spinners to the appropriate enum value.
-     *
-     * @param text The text to be converted.
-     * @return The enum value that corresponds to the text.
-     */
-    public Unit convertTextToUnit(String text) {
-        if (text == null || text.isEmpty()) {
-            return null;
-        }
-
-        switch (text) {
-            case "Meters":
-                return Unit.METER;
-            case "Kilometers":
-                return Unit.KILOMETER;
-            case "Centimeters":
-                return Unit.CENTIMETER;
-            case "Millimeters":
-                return Unit.MILLIMETER;
-            case "Miles":
-                return Unit.MILE;
-            case "Yards":
-                return Unit.YARD;
-            case "Feet":
-                return Unit.FOOT;
-            case "Inches":
-                return Unit.INCH;
-            case "Nautical Miles":
-                return Unit.NAUTICAL_MILE;
-            default:
-                return null;
-        }
-    }
-
-    /**
-     * This enum is used to represent the different units of measurement
-     * that are available for conversion.
-     */
-    enum Unit {
-        METER, KILOMETER, CENTIMETER, MILLIMETER, MILE, YARD, FOOT, INCH, NAUTICAL_MILE
     }
 }
